6d5418bfb5c3f7be73e21f15a1c20c5c0866fd4a
喻培行 <peihang0226@163.com>
2023-07-18 18:03:06 +0800

    111

--- component-modal-yupeihang/src/plugins/modal/Modal.vue
+++ component-modal-yupeihang/src/plugins/modal/Modal.vue
@@ -1,1 +1,14 @@
-// 基础组件
+<!-- 基础组件 -->
+
+<template>
+  <Teleport to="body">
+    <!-- 蒙层 -->
+    <div class="wrapper">
+      <Transition>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Transition>
+    </div>
+  </Teleport>
+</template>
