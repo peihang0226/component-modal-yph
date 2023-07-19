53d4a1feeb8258fd056fd290f98eb6dfe05a6101
喻培行 <peihang0226@163.com>
2023-07-19 20:12:40 +0800

    样式

--- component-modal-yupeihang/src/plugins/modal/Modal.vue
+++ component-modal-yupeihang/src/plugins/modal/Modal.vue
@@ -5,10 +5,40 @@
     <!-- 蒙层 -->
     <div class="wrapper">
       <Transition>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div class="modal">
+          <div class="title">
+          {{ props.title }}
+          </div>
+          <div class="content">
+            {{ props.content }}
+          </div>
+          <div class="footer">
+            <button @click="throwSet('cancel',$event)" ref="butCancel">{{ props.cancelText }}</button>
+            <button @click="throwSet('ok',$event)" ref="butOk">{{ props.okText }}</button>
+          </div>
+        </div>
       </Transition>
     </div>
   </Teleport>
 </template>
+
+<style scoped>
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  height: 0;
+  overflow: hidden;
+  background: rgba(0, 0, 0, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal {
+  width: 200px;
+  height: 200px;
+  background: #fff;
+}
+
+</style>
